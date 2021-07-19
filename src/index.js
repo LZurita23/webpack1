7dafb0474dfd89d08c2f3128c1c86dbc2acac1be
Lori Zurita <lorizurita23@gmail.com>
2021-07-19 11:39:25 -0500

    add an image to the background

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import themessage from "./message.js";
 import './css/coffee.css';
 import './css/colors.css';
 import './css/main.css';
+import Icon from './icon.png';
 
 const title = document.getElementById("title");
 
 title.innerHTML = pascalCase(themessage);
 
- 
+// Add the image to our existing div.
+const myIcon = new Image();
+myIcon.src = Icon;
+
+element.appendChild(myIcon);
